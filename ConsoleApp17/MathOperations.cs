825c88f45cc49209042cb6207d5234bff8cfccb8
totoqqwe <anton.stepanchenko14@gmail.com>
2023-11-23 20:32:38 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp17/MathOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class MathOperations
+    {
+    public static T Add<T>(T a, T b)
+        {
+            dynamic dynamicA = a;
+            dynamic dynamicB = b;
+            return dynamicA + dynamicB;
+        }
+
+        public static T[] Add<T>(T[] arr1, T[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+                throw new ArgumentException("Arrays must have the same length.");
+
+            T[] result = new T[arr1.Length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                result[i] = Add(arr1[i], arr2[i]);
+            }
+            return result;
+        }
+
+        public static T[,] Add<T>(T[,] matrix1, T[,] matrix2)
+        {
+            int rows = matrix1.GetLength(0);
+            int cols = matrix1.GetLength(1);
+
+            if (rows != matrix2.GetLength(0) || cols != matrix2.GetLength(1))
+                throw new ArgumentException("Matrices must have the same dimensions.");
+
+            T[,] result = new T[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = Add(matrix1[i, j], matrix2[i, j]);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5, num2 = 10;
+            Console.WriteLine($"Addition of numbers: {MathOperations.Add(num1, num2)}");
+
+            double[] arr1 = { 1.5, 2.5, 3.5 };
+            double[] arr2 = { 0.5, 1.5, 2.5 };
+            var resultArr = MathOperations.Add(arr1, arr2);
+            Console.WriteLine($"Addition of arrays: [{string.Join(", ", resultArr)}]");
+
+            int[,] matrix1 = { { 1, 2 }, { 3, 4 } };
+            int[,] matrix2 = { { 5, 6 }, { 7, 8 } };
+            var resultMatrix = MathOperations.Add(matrix1, matrix2);
+            Console.WriteLine("Addition of matrices:");
+            for (int i = 0; i < resultMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < resultMatrix.GetLength(1); j++)
+                {
+                    Console.Write($"{resultMatrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    }
+}
